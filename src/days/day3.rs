7850b1a1ef14a6aab89376656681651d50f5c70c
julesjung <94782485+julesjung@users.noreply.github.com>
2025-12-03 17:56:23 +0100

    feat: solve day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 use crate::utils::{Answers, read};
 
 #[inline]
-fn joltage(rating: &str) -> u32 {
+fn part1_joltage(rating: &str) -> u32 {
     let mut first_digit = '0';
     let mut last_digit = '0';
     for digit in rating.chars() {
@@ -21,16 +21,39 @@ fn joltage(rating: &str) -> u32 {
     first_digit * 10 + last_digit
 }
 
+#[inline]
+fn part2_joltage(rating: &str) -> u64 {
+    let mut digits: [char; 12] = ['0'; 12];
+    for (index, digit) in rating.chars().enumerate() {
+        let mut current_index = (12 + index).saturating_sub(rating.len());
+        while current_index < 12 {
+            if digits[current_index] < digit {
+                digits[current_index] = digit;
+                for item in digits.iter_mut().skip(current_index + 1) {
+                    *item = '0';
+                }
+                break;
+            }
+            current_index += 1;
+        }
+    }
+
+    let joltage: String = digits.iter().collect();
+
+    joltage.parse().unwrap()
+}
+
 pub fn run(input: &str) -> io::Result<Answers> {
     let input = read(input)?;
 
     let mut part1: u32 = 0;
-    let mut part2: u32 = 0;
+    let mut part2: u64 = 0;
 
     for line in input.lines() {
         let rating = line.unwrap();
-        part1 += joltage(rating.as_str());
+        part1 += part1_joltage(rating.as_str());
+        part2 += part2_joltage(rating.as_str());
     }
 
-    Ok((Some(part1 as u64), None))
+    Ok((Some(part1 as u64), Some(part2)))
 }
